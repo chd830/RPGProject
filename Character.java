983d5fef80fac27cffece770453c85e3f08c1462
chong <chd830@naver.com>
2019-07-30 14:29:13 +0900

    자식에서 중복되는 코드 부모에서 처리할 수 있게 올림. 매개변수의 회피와 공격을 없애고 스킬의 반환타입을 불린으로 바꿈. 스킬이 true일때 데미지가 더 들어가게 바꿀것.

--- Character.java
+++ Character.java
@@ -2,6 +2,8 @@ package com.project1;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Character implements Motion, Serializable {
 	/**
@@ -20,7 +22,7 @@ public class Character implements Motion, Serializable {
 	private int evasion;
 	private boolean isAlive;
 	List<String> item = new ArrayList<String>();
-	
+
 	public int getLevel() {
 		return level;
 	}
@@ -41,7 +43,7 @@ public class Character implements Motion, Serializable {
 	public void setMaxHP(int maxHP) {
 		MaxHP = maxHP;
 	}
-	
+
 	public int getMP() {
 		return MP;
 	}
@@ -55,7 +57,7 @@ public class Character implements Motion, Serializable {
 	public void setMaxMP(int maxMP) {
 		MaxMP = maxMP;
 	}
-	
+
 	public int getAttack() {
 		return attack;
 	}
@@ -69,36 +71,36 @@ public class Character implements Motion, Serializable {
 	public void setEvasion(int evasion) {
 		this.evasion = evasion;
 	}	
-	
-	
+
+
 	public boolean getIsAlive() {
 		return isAlive;
 	}
 	public void setIsAlive(boolean isAlive) {
 		this.isAlive = isAlive;
 	}
-	
+
 	public int getCritical() {
 		return critical;
 	}
 	public void setCritical(int critical) {
 		this.critical = critical;
 	}
-	
+
 	public boolean isAlive() {
 		return isAlive;
 	}
 	public void setAlive(boolean isAlive) {
 		this.isAlive = isAlive;
 	}
-	
+
 	public int[] getExperience() {
 		return experience;
 	}
 	public void setExperience(int[] experience) {
 		this.experience = experience;
 	}
-	
+
 	public List<String> getItem() {
 		return item;
 	}
@@ -106,34 +108,218 @@ public class Character implements Motion, Serializable {
 		this.item = item;
 	}
 	public void getItemByMonster(Object o1, List<String> item) {
+		Random rand = new Random();
+		Monster m = null;
+		String str = "";
+		int num = rand.nextInt(10);
+		if(num < 3) {
+			System.out.print("Get \'Hp up\'");
+			item.add("Hp up");
+		}
+		else if(num < 6) {
+			System.out.print("Get \'Mp up\'");
+			item.add("Mp up");
+		}
+		else {
+			System.out.print("Get \'Iced\'");
+			item.add("Iced");
+		}
+		if(o1.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime)o1;
+			str = "Slime";
+
+		}
+		System.out.print(" from "+str+"\n");
 	}
 	@Override
 	public void useItem(Object o1, Object o2, List<String> item) {
+		String str = "";
+		Character c = null;
+		if(o1.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer)o1;
+			str = "Archer";
+		}
+		else if(o1.getClass().getName().equals("com.project1.Warrior")) {
+			c = (Warrior)o1;
+			str = "Warrior";
+		}
+		else {
+			c = (Magician)o1;
+			str = "Magician";
+		}
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Select Item: \t");
+		for(int i = 0; i < item.size(); i++) {
+			System.out.print((i+1)+"."+item.get(i)+"\t");
+		}
+		int num = sc.nextInt();
+		System.out.println(item.get(num - 1)+"\' is selected");
+		if(item.get(num - 1).equals("Hp up")) {
+			int cur = c.getHP();
+			c.setHP(cur + 10);
+			System.out.println("Hp of the "+str + " is become "+cur + " to "+c.getHP());
+		}
+		else if(item.get(num - 1).equals("Mp up")) {
+			int cur = c.getMP();
+			c.setMP(cur + 10);
+			System.out.println("Mp of the "+str + " is become "+cur + " to "+c.getMP());
+		}
+		else {
+			if(o2.getClass().getName().equals("com.project1.Slime")) {
+				Slime s = (Slime)o2;
+				s.setStatus("Iced");
+				System.out.println("Slime is Iced.");
+			}
+		}
+		item.remove(num - 1);
 	}
+	
 	@Override
-	public void attack(Object o1, Object o2, int attack) {
+	public void attack(Object o1, Object o2) {
+		Monster m = null;
+		Character c = null;
+		if(o1.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer)o1;
+		}
+		else if(o1.getClass().getName().equals("com.project1.Warrior")) {
+			c = (Warrior)o1;
+		}
+		else {
+			c = (Magician)o1;
+		}
+		String str = "";
+		int attack = c.getAttack();
+		if(o2.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime)o2;
+			str = "Slime";
+		}
+		int cur = m.getHP();
+		if(criticalJudgement(c.getCritical())) {
+			attack *= 2;
+			System.out.println("Critical damage!");
+		}
+		m.setHP(cur - attack);
+		if(m.getHP() <= 0) {
+			m.setHP(0);
+		}
+		System.out.println(str +" was attacked and became " + m.getHP() + "HP.");
+		if (m.getHP() == 0) {
+			isAlive(m);
+		}
 	}
+	
 	@Override
-	public void attackJudgement(Object o1, Object o2, int evasion) {
+	public void attackJudgement(Object o1, Object o2) {
+		Random rand = new Random();
+		Monster m = null;
+		String str = "";
+		int num = rand.nextInt(100);
+		if(o2.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime)o2;
+			str = "Slime";
+
+		}
+		if(num >= (100 - m.getEvasion())) {
+			System.out.println(str + " succeeded in evasion and became "+m.getHP()+"HP.");
+			return;
+		}
+		attack(o1, m);	
 	}
+	
 	@Override
 	public boolean criticalJudgement(int critical) {
+		Random rand = new Random();
+		int num = rand.nextInt(100);
+		if(num > (100 - critical)) {
+			return true;
+		}
 		return false;
 	}
+	
 	@Override
 	public void print(Object o) {
-	}
-	@Override
-	public boolean isAlive(Object o) {
-		return false;
-	}
-	@Override
-	public void skill(Object o1, Object o2, int attack) {
+		Character c = null;
+		System.out.println();
+		if(o.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer)o;
+			System.out.println("궁수의 레벨: "+c.getLevel());
+		}
+		else if(o.getClass().getName().equals("com.project1.Warrior")) {
+			System.out.println("전사의 레벨: "+c.getLevel());
+			c = (Warrior)o;
+		}
+		else {
+			c = (Magician)o;
+			System.out.println("마법사의 레벨: "+c.getLevel());
+		}
+		System.out.println("HP: " +c.getHP()+ ", MP: " +c.getMP());
+		System.out.println("공격력: " +c.getAttack());
+		System.out.println("회피율: " +c.getEvasion());
+		System.out.println("치명타율: " +c.getCritical()+"\n");
 	}
 	
+	@Override
+	public void isAlive(Object o) {
+		Monster c = null;
+		if(o.getClass().getName().equals("com.project1.Slime")) {
+			c = (Slime)o;
+			System.out.println("Slime is dead.");
+		}
+		c.setIsAlive(false);
+	}
+	
+	@Override
+	public boolean skill(Object o1, Object o2) {
+		System.out.println("use Skill");
+		return true;
+	}
+
 	public void showStatus(Object o1, Object o2) {
+		Character c = null;
+		if(o1.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer)o1;
+		}
+		else if(o1.getClass().getName().equals("com.project1.Warrior")) {
+			c = (Warrior)o1;
+		}
+		else {
+			c = (Magician)o1;
+		}
+		Monster m = null;
+		String str = "";
+		int hp = c.getHP();
+		int mp = c.getMP();
+		if(o2.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime)o2;
+			str = "Slime";
+		}
+		System.out.println("\nArcher");
+		System.out.print("HP: ");
+		for(int i = 0; i < c.getHP()/10; i++) {
+			System.out.print("■");
+		}
+		for(;hp < c.getMaxHP();hp += 10) {
+			System.out.print("□");
+		}
+		System.out.print("\nMP: ");
+		for(int i = 0; i < c.getMP()/10; i++) {
+			System.out.print("■");
+		}
+		for(;c.getMP() < c.getMaxMP();mp+=10) {
+			System.out.print("□");
+		}
+		System.out.println("\n\n"+str);
+		System.out.print("HP: ");
+		for(int i = 0; i < m.getHP()/10; i++) {
+			System.out.print("■");
+		}
+		hp = m.getHP();
+		for(;hp < m.getMaxHP();hp += 10) {
+			System.out.print("□");
+		}
+		System.out.println("");
 	}
 	transient List<Character> characterList = new ArrayList();
-	
+
 
 }
--- Slime.java
+++ Slime.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Slime extends Monster {
 	Random rand = new Random();
 	static int count = 0;
-	
+
 	public Slime() {
 		setLevel(1);
 		setHP(100);
@@ -15,16 +15,11 @@ public class Slime extends Monster {
 		setIsAlive(true);
 		System.out.println("You met slime");
 	}
-<<<<<<< HEAD
-	//°ø°ÝÇÏ´Â ¸Þ¼­µå. ¸ó½ºÅÍ, Ä³¸¯ÅÍ, ¸ó½ºÅÍÀÇ °ø°Ý·Â
-=======
-
-	//ï¿½ï¿½ï¿½ï¿½ï¿½Ï´ï¿½ ï¿½Þ¼ï¿½ï¿½ï¿½. ï¿½ï¿½ï¿½ï¿½, Ä³ï¿½ï¿½ï¿½ï¿½, ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ý·ï¿½
->>>>>>> chong
 	@Override
-	public void attack(Object o1, Object o2, int attack) {
+	public void attack(Object o1, Object o2) {
 		Character c = null;
 		String str = "";
+		int attack = ((Slime)o1).getAttack();
 		if(o2.getClass().getName().equals("com.project1.Warrior")) {
 			str = "Warrior";
 			c = (Warrior)o2;
@@ -44,54 +39,50 @@ public class Slime extends Monster {
 		}
 	}
 
-	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ È¸ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Ã¼Å© ï¿½Þ¼ï¿½ï¿½ï¿½. ï¿½ï¿½ï¿½ï¿½, Ä³ï¿½ï¿½ï¿½ï¿½, ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ È¸ï¿½ï¿½
 	@Override
-	public void attackJudgement(Object o1, Object o2, int evasion) {
+	public void attackJudgement(Object o1, Object o2) {
 		Monster m = (Slime)o1;
-		//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
-		try {
-			if(m.getStatus().equals("Iced") && count < 1) {
-				count++;
-				return;
-			}
-			else if(m.getStatus().equals("Fired")&& count < 1) {
-				count++;
-				System.out.println("Slime is in Fired status");
-				attack(o2, m, 2);
-				return;
-			}
-		} catch(Exception e) {
+		if(m.getStatus().equals("Iced") && count < 1) {
+			count++;
+			return;
 		}
-		
+		else if(m.getStatus().equals("Fired")&& count < 1) {
+			count++;
+			System.out.println("Slime is in Fired status");
+			attack(o2, m);
+			return;
+		}
+
 		m.setStatus("");
 		count = 0;
+		
 		Random rand = new Random();
 		Character c = null;
 		int num = rand.nextInt(100);
 		if(o2.getClass().getName().equals("com.project1.Warrior")) {
 			c = (Warrior)o2;
-			if(num >= (100 - evasion)) {
+			if(num >= (100 - m.getAttack())) {
 				System.out.println("Warrior succeeded in evasion and became "+c.getHP()+"HP.");
 				return;
 			}
 		} else if(o2.getClass().getName().equals("com.project1.Magician")) {
 			c = (Magician)o2;
 			num+=100;
-			if(num >= (100 - evasion)) {
+			if(num >= (100 - m.getEvasion())) {
 				System.out.println("Magician succeeded in evasion and became "+c.getHP()+"HP.");
 			}
 		} else {
 			c = (Archer)o2;
 			num+=100;
-			if(num >= (100 - evasion)) {
+			if(num >= (100 - m.getEvasion())) {
 				System.out.println("Archer succeeded in evasion and became "+c.getHP()+"HP.");
 			}
 		}
-		attack(o2, m, c.getAttack());
+		attack(o2, m);
 	}
 
 	@Override
-	public boolean isAlive(Object o) {
+	public void isAlive(Object o) {
 		Character c = null;
 		if(o.getClass().getName().equals("com.project1.Warrior")) {
 			c = (Warrior)o;
@@ -106,7 +97,6 @@ public class Slime extends Monster {
 			System.out.println("Archer is dead.");
 		}
 		c.setIsAlive(false);
-		return false;
 	}
 
 }
