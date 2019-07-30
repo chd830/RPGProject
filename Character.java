1b473cbba16799d7a533fdc03dc2ba6f21fa8147
chlgydms7 <chlgydms7@gmail.com>
2019-07-30 18:15:50 +0900

    스레드

--- Character.java
+++ Character.java
@@ -1,14 +1,16 @@
 package com.project1;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Character implements Motion, Serializable, Runnable {
+public class Character implements Motion, Serializable {
+
 	/**
-	 * Serialized error in scanner >> Need to use transient
-	 * Cannot find some class >> Need to fix serialVersionUID
+	 * Serialized error in scanner >> Need to use transient Cannot find some class
+	 * >> Need to fix serialVersionUID
 	 */
 	private static final long serialVersionUID = 4379276376446092084L;
 	private int level;
@@ -17,7 +19,7 @@ public class Character implements Motion, Serializable, Runnable {
 	private int MP;
 	private int MaxMP;
 	private int attack;
-	private int[] experience = {5, 10, 20, 20, 20};
+	private int[] experience = { 5, 10, 20, 20, 20 };
 	private int critical;
 	private int evasion;
 	private boolean isAlive;
@@ -26,6 +28,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getLevel() {
 		return level;
 	}
+
 	public void setLevel(int level) {
 		this.level = level;
 	}
@@ -33,6 +36,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getHP() {
 		return HP;
 	}
+
 	public void setHP(int hP) {
 		HP = hP;
 	}
@@ -40,6 +44,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getMaxHP() {
 		return MaxHP;
 	}
+
 	public void setMaxHP(int maxHP) {
 		MaxHP = maxHP;
 	}
@@ -47,6 +52,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getMP() {
 		return MP;
 	}
+
 	public void setMP(int mP) {
 		MP = mP;
 	}
@@ -54,6 +60,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getMaxMP() {
 		return MaxMP;
 	}
+
 	public void setMaxMP(int maxMP) {
 		MaxMP = maxMP;
 	}
@@ -61,6 +68,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getAttack() {
 		return attack;
 	}
+
 	public void setAttack(int attack) {
 		this.attack = attack;
 	}
@@ -68,13 +76,15 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getEvasion() {
 		return evasion;
 	}
+
 	public void setEvasion(int evasion) {
 		this.evasion = evasion;
-	}	
+	}
 
 	public boolean getIsAlive() {
 		return isAlive;
 	}
+
 	public void setIsAlive(boolean isAlive) {
 		this.isAlive = isAlive;
 	}
@@ -82,6 +92,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int getCritical() {
 		return critical;
 	}
+
 	public void setCritical(int critical) {
 		this.critical = critical;
 	}
@@ -89,6 +100,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public boolean isAlive() {
 		return isAlive;
 	}
+
 	public void setAlive(boolean isAlive) {
 		this.isAlive = isAlive;
 	}
@@ -96,6 +108,7 @@ public class Character implements Motion, Serializable, Runnable {
 	public int[] getExperience() {
 		return experience;
 	}
+
 	public void setExperience(int[] experience) {
 		this.experience = experience;
 	}
@@ -103,47 +116,46 @@ public class Character implements Motion, Serializable, Runnable {
 	public List<String> getItem() {
 		return item;
 	}
+
 	public void setItem(List<String> item) {
 		this.item = item;
 	}
+
 	public void getItemByMonster(Object o1, List<String> item) {
 		Random rand = new Random();
 		Monster m = null;
 		String str = "";
 		int num = rand.nextInt(10);
-		if(num < 3) {
+		if (num < 3) {
 			System.out.print("Get \'Hp up\'");
 			item.add("Hp up");
-		}
-		else if(num < 6) {
+		} else if (num < 6) {
 			System.out.print("Get \'Mp up\'");
 			item.add("Mp up");
-		}
-		else {
+		} else {
 			System.out.print("Get \'Iced\'");
 			item.add("Iced");
 		}
-		if(o1.getClass().getName().equals("com.project1.Slime")) {
-			m = (Slime)o1;
+		if (o1.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime) o1;
 			str = "Slime";
 
 		}
-		System.out.print(" from "+str+"\n");
+		System.out.print(" from " + str + "\n");
 	}
+
 	@Override
 	public void useItem(Object o1, Object o2, List<String> item) {
 		String str = "";
 		Character c = null;
-		if(o1.getClass().getName().equals("com.project1.Archer")) {
-			c = (Archer)o1;
+		if (o1.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer) o1;
 			str = "Archer";
-		}
-		else if(o1.getClass().getName().equals("com.project1.Warrior")) {
-			c = (Warrior)o1;
+		} else if (o1.getClass().getName().equals("com.project1.Warrior")) {
+			c = (Warrior) o1;
 			str = "Warrior";
-		}
-		else {
-			c = (Magician)o1;
+		} else {
+			c = (Magician) o1;
 			str = "Magician";
 		}
 		Scanner sc = new Scanner(System.in);
@@ -155,26 +167,26 @@ public class Character implements Motion, Serializable, Runnable {
 		}
 		
 		System.out.print("Select Item: \t");
-		for(int i = 0; i < item.size(); i++) {
-			
-			try {
-				Thread.sleep(300);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-			
-			System.out.print((i+1)+"."+item.get(i)+"\t:");
-		}
-		int num = sc.nextInt();
 		
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(300);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		
-		System.out.println(item.get(num - 1)+"\' is selected");
-		if(item.get(num - 1).equals("Hp up")) {
+		for (int i = 0; i < item.size(); i++) {
+			System.out.print((i + 1) + "." + item.get(i) + "\t : ");
+		}
+		int num = sc.nextInt();
+		
+		try {
+			Thread.sleep(500);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		System.out.println(item.get(num - 1) + "\' is selected");
+		if (item.get(num - 1).equals("Hp up")) {
 			int cur = c.getHP();
 			c.setHP(cur + 10);
 			
@@ -184,23 +196,20 @@ public class Character implements Motion, Serializable, Runnable {
 				// TODO: handle exception
 			}
 			
-			System.out.println("Hp of the "+str + " is become "+cur + " to "+c.getHP());
-		}
-		else if(item.get(num - 1).equals("Mp up")) {
+			System.out.println("Hp of the " + str + " is become " + cur + " to " + c.getHP());
+		} else if (item.get(num - 1).equals("Mp up")) {
 			int cur = c.getMP();
 			c.setMP(cur + 10);
-			
 			try {
 				Thread.sleep(500);
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
 			
-			System.out.println("Mp of the "+str + " is become "+cur + " to "+c.getMP());
-		}
-		else {
-			if(o2.getClass().getName().equals("com.project1.Slime")) {
-				Slime s = (Slime)o2;
+			System.out.println("Mp of the " + str + " is become " + cur + " to " + c.getMP());
+		} else {
+			if (o2.getClass().getName().equals("com.project1.Slime")) {
+				Slime s = (Slime) o2;
 				s.setStatus("Iced");
 				
 				try {
@@ -214,145 +223,120 @@ public class Character implements Motion, Serializable, Runnable {
 		}
 		item.remove(num - 1);
 	}
-	
+
 	@Override
 	public void attack(Object o1, Object o2) {
 		Monster m = null;
 		Character c = null;
-		if(o1.getClass().getName().equals("com.project1.Archer")) {
-			c = (Archer)o1;
-		}
-		else if(o1.getClass().getName().equals("com.project1.Warrior")) {
-			c = (Warrior)o1;
-		}
-		else {
-			c = (Magician)o1;
+		if (o1.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer) o1;
+		} else if (o1.getClass().getName().equals("com.project1.Warrior")) {
+			c = (Warrior) o1;
+		} else {
+			c = (Magician) o1;
 		}
 		String str = "";
 		int attack = c.getAttack();
-		if(o2.getClass().getName().equals("com.project1.Slime")) {
-			m = (Slime)o2;
+		if (o2.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime) o2;
 			str = "Slime";
-		}else if(o2.getClass().getName().equals("com.project1.Boss")) {
-			m = (Boss)o2;
+		} else if (o2.getClass().getName().equals("com.project1.Boss")) {
+			m = (Boss) o2;
 			str = "Boss";
 		}
 		int cur = m.getHP();
-		if(criticalJudgement(c.getCritical())) {
+		if (criticalJudgement(c.getCritical())) {
 			attack *= 2;
-			System.out.println("Critical damage!");
+			try {
+				System.out.println("Critical damage!");
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 		}
 		m.setHP(cur - attack);
-		if(m.getHP() <= 0) {
+		if (m.getHP() <= 0) {
 			m.setHP(0);
 		}
-		
 		try {
-			Thread.sleep(700);
+
+			System.out.println(str + " was attacked and became " + m.getHP() + "HP.");
+			Thread.sleep(1000);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-		System.out.println(str +" was attacked and became " + m.getHP() + "HP.");
 		if (m.getHP() == 0) {
 			isAlive(m);
 		}
 	}
-	
+
 	@Override
 	public void attackJudgement(Object o1, Object o2) {
 		Random rand = new Random();
 		Monster m = null;
 		String str = "";
 		int num = rand.nextInt(100);
-		if(o2.getClass().getName().equals("com.project1.Slime")) {
-			m = (Slime)o2;
+		if (o2.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime) o2;
 			str = "Slime";
 
-		}else if(o2.getClass().getName().equals("com.project1.Boss")) {
-			m = (Boss)o2;
+		} else if (o2.getClass().getName().equals("com.project1.Boss")) {
+			m = (Boss) o2;
 			str = "Boss";
 		}
-		if(num >= (100 - m.getEvasion())) {
-			
-			try {
-				Thread.sleep(700);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-			
-			System.out.println(str + " succeeded in evasion and became "+m.getHP()+"HP.");
+		if (num >= (100 - m.getEvasion())) {
+
+			System.out.println(str + " succeeded in evasion and became " + m.getHP() + "HP.");
+
 			return;
+
 		}
-		attack(o1, m);	
+		attack(o1, m);
 	}
-	
+
 	@Override
 	public boolean criticalJudgement(int critical) {
 		Random rand = new Random();
 		int num = rand.nextInt(100);
-		if(num > (100 - critical)) {
+		if (num > (100 - critical)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	@Override
 	public void print(Object o) {
 		Character c = null;
 		System.out.println();
-		
-		try {
-			Thread.sleep(500);
-		} catch (Exception e) {
-			// TODO: handle exception
+		if (o.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer) o;
+			System.out.println("Archer's level: " + c.getLevel());
+		} else if (o.getClass().getName().equals("com.project1.Warrior")) {
+			c = (Warrior) o;
+			System.out.println("Warrior's level: " + c.getLevel());
+		} else {
+			c = (Magician) o;
+			System.out.println("Magician's level: " + c.getLevel());
 		}
-		
-		if(o.getClass().getName().equals("com.project1.Archer")) {
-			c = (Archer)o;
-			System.out.println("Archer's level: "+c.getLevel());
-		}
-		else if(o.getClass().getName().equals("com.project1.Warrior")) {
-			c = (Warrior)o;
-			System.out.println("Warrior's level: "+c.getLevel());
-		}
-		else {
-			c = (Magician)o;
-			System.out.println("Magician's level: "+c.getLevel());
-		}
-		System.out.println("HP: " +c.getHP()+ ", MP: " +c.getMP());
-		System.out.println("Attack: " +c.getAttack());
-		System.out.println("Evasion: " +c.getEvasion());
-		System.out.println("Critical: " +c.getCritical()+"\n");
+		System.out.println("HP: " + c.getHP() + ", MP: " + c.getMP());
+		System.out.println("Attack: " + c.getAttack());
+		System.out.println("Evasion: " + c.getEvasion());
+		System.out.println("Critical: " + c.getCritical() + "\n");
 	}
-	
+
 	@Override
 	public void isAlive(Object o) {
 		Monster c = null;
-		if(o.getClass().getName().equals("com.project1.Slime")) {
-			c = (Slime)o;
-			
-			try {
-				Thread.sleep(700);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-			
+		if (o.getClass().getName().equals("com.project1.Slime")) {
+			c = (Slime) o;
 			System.out.println("Slime is dead.");
-		}else if(o.getClass().getName().equals("com.project1.Boss")) {
-			c = (Boss)o;
-			
-			try {
-				Thread.sleep(700);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-			
+		} else if (o.getClass().getName().equals("com.project1.Boss")) {
+			c = (Boss) o;
 			System.out.println("Boss is dead.");
 		}
 		c.setIsAlive(false);
 	}
-	
+
 	@Override
 	public boolean skill(Object o1, Object o2) {
 		System.out.println("use Skill");
@@ -362,67 +346,64 @@ public class Character implements Motion, Serializable, Runnable {
 	public void showStatus(Object o1, Object o2) {
 		Character c = null;
 		String s = "";
-		if(o1.getClass().getName().equals("com.project1.Archer")) {
-			c = (Archer)o1;
+		if (o1.getClass().getName().equals("com.project1.Archer")) {
+			c = (Archer) o1;
 			s = "Archer";
-		}
-		else if(o1.getClass().getName().equals("com.project1.Warrior")) {
-			c = (Warrior)o1;
+		} else if (o1.getClass().getName().equals("com.project1.Warrior")) {
+			c = (Warrior) o1;
 			s = "Warrior";
-		}
-		else {
-			c = (Magician)o1;
+		} else {
+			c = (Magician) o1;
 			s = "Magician";
 		}
 		Monster m = null;
 		String str = "";
 		int hp = c.getHP();
 		int mp = c.getMP();
-		if(o2.getClass().getName().equals("com.project1.Slime")) {
-			m = (Slime)o2;
+		if (o2.getClass().getName().equals("com.project1.Slime")) {
+			m = (Slime) o2;
 			str = "Slime";
-		}else if(o2.getClass().getName().equals("com.project1.Boss")) {
-			m = (Boss)o2;
+		} else if (o2.getClass().getName().equals("com.project1.Boss")) {
+			m = (Boss) o2;
 			str = "Boss";
 		}
-		
 		try {
+
 			Thread.sleep(1000);
+			
+			System.out.println("\n" + s);
+
+			System.out.print("HP: ");
+			for (int i = 0; i < c.getHP() / 10; i++) {
+				System.out.print("■");
+			}
+
+			for (; hp < c.getMaxHP(); hp += 10) {
+				System.out.print("□");
+			}
+			System.out.print("\nMP: ");
+			for (int i = 0; i < c.getMP() / 10; i++) {
+				System.out.print("■");
+			}
+			for (; c.getMP() < c.getMaxMP(); mp += 10) {
+				System.out.print("□");
+			}
+
+			System.out.println("\n\n" + str);
+			System.out.print("HP: ");
+			for (int i = 0; i < m.getHP() / 10; i++) {
+				System.out.print("■");
+			}
+			hp = m.getHP();
+			for (; hp < m.getMaxHP(); hp += 10) {
+				System.out.print("□");
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-		System.out.println("\n" +s);
-		System.out.print("HP: ");
-		for(int i = 0; i < c.getHP()/10; i++) {
-			System.out.print("■");
-		}
-		for(;hp < c.getMaxHP();hp += 10) {
-			System.out.print("□");
-		}
-		System.out.print("\nMP: ");
-		for(int i = 0; i < c.getMP()/10; i++) {
-			System.out.print("■");
-		}
-		for(;c.getMP() < c.getMaxMP();mp+=10) {
-			System.out.print("□");
-		}
-		System.out.println("\n\n"+str);
-		System.out.print("HP: ");
-		for(int i = 0; i < m.getHP()/10; i++) {
-			System.out.print("■");
-		}
-		hp = m.getHP();
-		for(;hp < m.getMaxHP();hp += 10) {
-			System.out.print("□");
-		}
 		System.out.println("");
 	}
+
 	transient List<Character> characterList = new ArrayList();
-
-	@Override
-	public void run() {
-	}
-
 
 }
--- Main.java
+++ Main.java
@@ -1,90 +1,61 @@
 package com.project1;
 
 import java.io.*;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 import com.project1.Character;
 
-public class Main implements Runnable {
+public class Main {
 	static List<Character> l = null;
+
 	public static void main(String[] args) throws Exception {
+
 		Scanner sc = new Scanner(System.in);
 		String str = "";
 		File f = new File("C:\\Users\\stu\\Desktop\\Project1\\data.txt");
 		Character c = null;
 		int num = 0;
-		int a,b;
-		System.out.println("-------------------------------------------");
-		for(a=1;a<=3;a++) {
-			System.out.print(" ");
-		}
-		for(b=1;b<=3;b++) {
-			System.out.print("*");
-		}
-		for(a=1;a<=5;a++) {
-			System.out.print(" ");
-		}
-		for(b=1;b<=3;b++) {
-			System.out.print("*");
-		}
-		for(a=1;a<=5;a++) {
-			System.out.print(" ");
-		}
-		for(b=1;b<=2;b++) {
-			System.out.print("*");
-		}
-		for(a=1;a<=3;a++) {
-			System.out.print(" ");
-		}
-		for(b=1;b<=2;b++) {
-			System.out.print("*");
-		}
-		for(a=1;a<=5;a++) {
-			System.out.print(" ");
-		}
-		for(b=1;b<=5;b++) {
-			System.out.print("*");
-		}
-		System.out.println();
-		System.out.println("-------------------------------------------");
-		if(f.exists()) {
+
+		if (f.exists()) {
 			System.out.print("Load Data?[y/n] ");
 			str = sc.next();
 		}
 
-		if(str.equals("y") || str.equals("Y")) {
+		if (str.equals("y") || str.equals("Y")) {
 			try {
 				set();
-				for(int i = 0; i < l.size(); i++) {
-					System.out.println((i + 1) + ".Lv" + l.get(i).getLevel() +" " + l.get(i).getClass().getName().substring(13, l.get(i).getClass().getName().length()));
+				for (int i = 0; i < l.size(); i++) {
+					System.out.println((i + 1) + ".Lv" + l.get(i).getLevel() + " "
+							+ l.get(i).getClass().getName().substring(13, l.get(i).getClass().getName().length()));
 				}
-			} catch(Exception e ) {
+			} catch (Exception e) {
 				System.out.println(e.toString());
 			}
-			
+
 			try {
 				Thread.sleep(500);
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
-			
+
 			System.out.print("Select: ");
 			num = sc.nextInt();
 			c = l.get(num - 1);
 
-		}
-		else {
-			l = new ArrayList(); 
+		} else {
+			l = new ArrayList();
 			System.out.print("1.Warrior 2.Magician 3.Archer: ");
 			num = sc.nextInt();
-			
+
 			try {
 				Thread.sleep(500);
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
 
-			switch(num) {
+			switch (num) {
 			case 1:
 				c = new Warrior();
 				break;
@@ -97,45 +68,44 @@ public class Main implements Runnable {
 			}
 		}
 
-
-
-		//Detailed data print
+		// Detailed data print
 		c.print(c);
+
 		Monster m = new Slime();
 
-		while(m.getIsAlive() && c.getIsAlive()) {
+		while (m.getIsAlive() && c.getIsAlive()) {
 
 			c.showStatus(c, m);
 
-			if(m.getHP() <= 0 || c.getHP() <= 0)
+			if (m.getHP() <= 0 || c.getHP() <= 0)
 				break;
-			recur:do {
+			recur: do {
 				try {
-					Thread.sleep(700);
+					Thread.sleep(1000);
+					System.out.print("\n1.Attack 2.UseItem 3.Skill: ");
 				} catch (Exception e) {
 					// TODO: handle exception
 				}
-				System.out.print("\n1.Attack 2.UseItem 3.Skill: ");
+
 				num = sc.nextInt();
-				switch(num) {
+				switch (num) {
 				case 1:
 					c.attackJudgement(c, m);
 					break recur;
 				case 2:
 					try {
-						
+
 						try {
-							Thread.sleep(500);
+							Thread.sleep(1000);
 						} catch (Exception e) {
 							// TODO: handle exception
 						}
-						
-						if(c.getItem().size() <= 0) {
+						if (c.getItem().size() <= 0) {
 							System.out.println("Item is null");
+
 							break;
 						}
-					}
-					catch(Exception e) {
+					} catch (Exception e) {
 						System.out.println(e.toString());
 					}
 					c.useItem(c, m, c.getItem());
@@ -144,70 +114,68 @@ public class Main implements Runnable {
 					c.skill(c, m);
 					break recur;
 				}
-			} while(true);
+			} while (true);
 			m.attack(m, c);
-		}
 
-		if(m.getHP() <= 0) {
-			c.getItemByMonster(m, c.item);
-			
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-			
-			System.out.print("1.Save 2.Exit: ");
-			num = sc.nextInt();
-			try {
-				if(num == 1) {
-					l.add(c);
-					save(l);
-				}else if(num==2) {
-					m = new Slime();
+			if (m.getHP() <= 0) {
+				c.getItemByMonster(m, c.item);
+				try {
+					Thread.sleep(1000);
+				} catch (Exception e) {
+					// TODO: handle exception
 				}
-			} catch(Exception e ) {
+
+				System.out.print("1.Save 2.Exit: ");
+				num = sc.nextInt();
+				try {
+					switch (num) {
+					case 1:
+						l.add(c);
+						save(l);
+						break;
+					case 2:
+						m = new Slime();
+						break;
+					}
+				} catch (Exception e) {
+
+				}
+			}
+			if (c.getHP() <= 0) {
+				try {
+					Thread.sleep(1000);
+					System.out.println("Game is end");
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
 
 			}
 		}
-		
-		try {
-			Thread.sleep(1000);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		if(c.getHP() <= 0) {
-			System.out.println("Game is end");
-		}
-
 	}
+
 	public static void save(List<Character> c) {
 		try {
 			FileOutputStream fos = new FileOutputStream("C:\\Users\\stu\\Desktop\\Project1\\data.txt");
-			//			FileOutputStream fos = new FileOutputStream("c:\\Users\\com\\Desktop\\RPGProject\\data.txt");
+			// FileOutputStream fos = new
+			// FileOutputStream("c:\\Users\\com\\Desktop\\RPGProject\\data.txt");
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(c);
 			oos.close();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
 	}
+
 	@SuppressWarnings("unchecked")
 	public static void set() {
 		FileInputStream fis;
 		try {
 			fis = new FileInputStream("C:\\Users\\stu\\Desktop\\Project1\\data.txt");
-			//			fis = new FileInputStream("c:\\Users\\com\\Desktop\\RPGProject\\data.txt");
+			// fis = new FileInputStream("c:\\Users\\com\\Desktop\\RPGProject\\data.txt");
 			ObjectInputStream ois = new ObjectInputStream(fis);
-			l = (List<Character>)ois.readObject();
+			l = (List<Character>) ois.readObject();
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
 	}
-	@Override
-	public void run() {
-	}
 }
-
-
