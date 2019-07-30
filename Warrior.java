b949ddd36f9890912bb6065409aa8bbffc2612c1
duektha <duektha@gmail.com>
2019-07-30 11:57:44 +0900

    공격/회피 판정 작성, 크리티컬 판정 작성

--- Warrior.java
+++ Warrior.java
@@ -1,169 +1,198 @@
 package com.project1;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
 
-public class Warrior extends Character implements Serializable{
-	Scanner sc = new Scanner(System.in);
-	Random rand = new Random();
+import com.project1.Character;
 
-	public Warrior() {
+public class Warrior extends Character implements Serializable {
 
-		setLevel(1);
-		setHP(100);
-		setMP(10);
-		setCritical(50);
-		setAttack(10);
-		setEvasion(10);
-		setIsAlive(true);
-		System.out.println("Warrior is selected");
-	}
+   private static final long serialVersionUID = 5716387998910136780L;
+   private static final String Warrior = null;
+   transient Scanner sc = new Scanner(System.in);
+   transient Random rand = new Random();
+   
+   public Warrior() {
 
-	//ƒ≥∏Ø≈Õ¿« ¡§∫∏ √‚∑¬ ∏ﬁº≠µÂ
-	@Override
-	public void print(Object o) {
-		Warrior w = (Warrior) o;
-		System.out.println();
-		System.out.println("Warrior's Level is: " + w.getLevel());
-		System.out.println("HP: " + w.getHP() + ", MP: " + w.getMP());
-		System.out.println("Attack: " + w.getAttack());
-		System.out.println("Evasion: " + w.getEvasion());
-		System.out.println("Critical: " + w.getCritical() + "\n");
+      setLevel(1);
+      setHP(100);
+      setMaxHP(this.getHP());
+      setMP(10);
+      setMaxMP(this.getMaxMP());
+      setCritical(100);
+      setAttack(10);
+      setEvasion(10);
+      setIsAlive(true);
+      System.out.println("Warrior is selected.");
+   }
 
+   @Override
+   public void print(Object o) {
+      Warrior w = (Warrior) o;
+      System.out.println();
+      System.out.println("Warrior's Level is: " + w.getLevel());
+      System.out.println("HP: " + w.getHP() + ", MP: " + w.getMP());
+      System.out.println("Attack: " + w.getAttack());
+      System.out.println("Evasion: " + w.getEvasion());
+      System.out.println("Critical: " + w.getCritical() + "\n");
+      
+   }
+   
 
-	}
+   @Override
+   public void useItem(Object o1, Object o2, List<String> item) {
+      Warrior w = (Warrior) o1;
+      if (item.size() < 0) {
+         System.out.println("Item is empty");
+         return;
+      }
+      System.out.print("Select Item: \t");
+      for(int i = 0; i < item.size(); i++) {
+         System.out.print((i+1)+"."+item.get(i)+"\t");
+      }
+      
+      int num = sc.nextInt();
+      System.out.println(item.get(num - 1)+"\' is selected");
+      if(item.get(num - 1).equals("Hp up")) {
+         int cur = w.getHP();
+         w.setHP(cur + 10);
+         System.out.println("Hp of the warrior is become " + cur + " to " + w.getHP());
+      } 
+      else if (item.get(num - 1).equals("Mp up")) {
+         int cur = w.getMP();
+         w.setMP(cur + 10);
+         System.out.println("Mp of the warrior is become " + cur + " to " + w.getMP());
+      }
+      else {
+         if (o2.getClass().getName().equals("com.project1.Slime")) {
+            Slime s = (Slime) o2;
+            s.setStatus("Iced");
+            System.out.println("Slime is Iced.");
+         }
+      }
+      item.remove(num - 1);
+   }
 
+   @Override
+   //Í≥µÍ≤©, ÌöåÌîº ÌåêÏ†ï
+   public void attackJudgement(Object o1, Object o2, int evasion) {
+      Random rand = new Random();
+      Monster m = null;
+      int num = rand.nextInt(100);
+      if(o2.getClass().getName().equals("com.project1.Slime")) {
+         m = (Slime)o2;
+      }
+      if(num >= (100 - evasion)) {
+         System.out.println("attack failure");
+         return;
+      }
+      attack(o1, m, ((Warrior)o1).getAttack());
+      //Ï∫êÎ¶≠ÌÑ∞, Î™¨Ïä§ÌÑ∞, Ï∫êÎ¶≠ÌÑ∞ Í≥µÍ≤©Î†•
+   }
 
-	//æ∆¿Ã≈€¿Ã ¿÷¥¬¡ˆ »Æ¿Œ«œ∞Ì æ∆¿Ã≈€¿ª ªÁøÎ«œ¥¬ ∏ﬁº≠µÂ
-	@Override
-	public void useItem(Object o1, Object o2, List<String> item) {
-		Warrior w = (Warrior) o1;
-		if (item.size() < 0) {
-			System.out.println("Item is empty");
-			return;
-		}
-		System.out.println("Select Item: ");
-		for (int i = 0; i < item.size(); i++) {
-			System.out.print((i + 1) + "." + item.get(i) + "\t");
-		}
-		int num = sc.nextInt();
-		System.out.println("\'" + item.get(num - 1) + "\' is selected");
-		if (item.get(num - 1).equals("Hp up")) {
-			int cur = w.getHP();
-			w.setHP(cur + 10);
-			System.out.println("Hp of the warrior is become " + cur + " to " + w.getHP());
-		} else if (item.get(num - 1).equals("Mp up")) {
-			int cur = w.getMP();
-			w.setMP(cur + 10);
-			System.out.println("Mp of the warrior is become " + cur + " to " + w.getMP());
-		} else {
-			if (o2.getClass().getName().equals("com.project1.Slime")) {
-				Slime s = (Slime) o2;
-				s.setStatus("Iced");
-				System.out.println("Slime is Iced.");
-			}
-		}
-		item.remove(num - 1);
-	}
-
-	//∞¯∞›¿Œ¡ˆ »∏««¿Œ¡ˆ ∆«¡§√º≈© ∏ﬁº≠µÂ
-	@Override
-	public void attackJudgement(Object o1, Object o2, int evasion) {
-		//ƒ≥∏Ø≈Õ, ∏ÛΩ∫≈Õ, ƒ≥∏Ø≈Õ¿« »∏««¿≤
-		Random rd = new Random();
-		int num = rd.nextInt(100);
-		Monster m =null;
-
-		//»∏««º∫∞¯
-		//o1 : com.Project1.Warrior
-		if(o1.getClass().getName().equals("com.project1.Slime")) {
-			m= (Slime)o2;
-		}
-		else if(o1.getClass().getName().equals("com.project1.Slime2")) {
-			m=(Slime)o2;
-		}
-		if(num>=(100-evasion)) {
-			//»∏«« Ω«∆–, ƒ≥∏Ø≈Õ∞° ∞¯∞›
-			System.out.println("evasion failure : "+getHP());
-			
-		}
-		else {
-			//»∏«« º∫∞¯
-			System.out.println("evasion success");
-			return;
-		}
-		attack(o1, m, getAttack());
-	}
-
-	//≈©∏Æ∆ºƒ√¿Ã ≈Õ¡ˆ¥¬¡ˆ »Æ¿Œ«œ¥¬ ∏ﬁº≠µÂ(«ˆ¿Á¥¬ π´¡∂∞« ≈©∏Æ∆ºƒ√ªÛ≈¬)
-	@Override 
-	public boolean criticalJudgement(int critical) {
+   @Override 
+   //ÌÅ¨Î¶¨Ìã∞Ïª¨ ÌåêÏ†ï
+   public boolean criticalJudgement(int critical) {
 		Random rd = new Random();
 		int num = rd.nextInt(100);
 		if(num >=(100-critical)) {
-
-			System.out.println("critical failure"+  getCritical());
-			return false;
-
+			return false;	
 		}else{
-			System.out.println("critical success" + getCritical());
-			critical = getAttack()*2;
+			System.out.println("critical success, " + "critical : "+getCritical());
 			return true;
 		}
-	}
+   }
+   
+   @Override
+   public void attack(Object o1, Object o2, int attack) {
+      Monster m = (Slime)o2;
+      int cur = m.getHP();
+      if(criticalJudgement(((Warrior)o1).getCritical())) {
+         attack *= 2;
+         System.out.println("Critical damage!");
+      }
+      m.setHP(cur - attack);
+      System.out.println("Slime was attacked and became "+m.getHP()+"HP.");
+      if(m.getHP() <= 0) {
+         isAlive(m);
+      }
+   }
 
-	//∞¯∞›∏ﬁº≠µÂ
-	@Override
-	public void attack(Object o1, Object o2, int attack) {
-		//ΩΩ∂Û¿”∏∏ ∞¯∞›¥Á«œµµ∑œ µ«æÓ¿÷¿Ω
-		Monster m = (Slime)o1;
-		int cur = m.getHP();
-		//≈©∏Æ∆ºƒ√¿Ã ≈Õ¡≥¿ª ∂ß µ•πÃ¡ˆ∞° µŒπË∑Œ µÈæÓ∞°∞‘ µ«æÓ¿÷¿Ω.
-		if(criticalJudgement(((Warrior)o2).getCritical())) {
-			attack *= 2;
-			System.out.println("Critical damage!");
-		}
-		m.setHP(cur - attack);
-		System.out.println("Slime was attacked and became " + m.getHP() + "HP.");
-		if (m.getHP() == 0) {
-			isAlive(m);
-		}
+   @Override
+   public void getItemByMonster(Object o1, List<String> item) {
+      Monster m = null;
+   
+      int num = rand.nextInt(10);
+      if(num < 3) {
+         System.out.print("Get \'Hp up\'");
+         item.add("Hp up");
+      }
+      else if(num < 6) {
+         System.out.print("Get \'Mp up\'");
+         item.add("Mp up");
+      }
+      else {
+         System.out.print("Get \'Iced\'");
+         item.add("Iced");
+      }
+      if(o1.getClass().getName().equals("com.project1.Slime")) {
+         m = (Slime)o1;
+         System.out.print(" from slime\n");
+      }
+   }
 
-	}
+   @Override
+   public boolean isAlive(Object o) {
+      Monster c = null;
+      if (o.getClass().getName().equals("com.project1.Slime")) {
+         c = (Slime) o;
+         System.out.println("Slime is dead.");
+      }
+      c.setIsAlive(false);
+      return false;
+   }   
+   
+   public void skill(Object o1, Object o2, int attack) {
+      System.out.println("Ï†ÑÏÇ¨ Ïä§ÌÇ¨ ÏÇ¨Ïö©");
+      attack(o1, o2, attack*2);
+   }
+   
+   public void showStatus(Object o1, Object o2) {
+      Warrior w = (Warrior)o1;
+      Monster m = null;
+      String str = "";
+      int hp = w.getHP();
+      int mp = w.getMP();
+      if(o2.getClass().getName().equals("com.project1.Slime")) {
+         m = (Slime)o2;
+         str = "Slime";
+      }
+      System.out.println("\nWarrior");
+      System.out.print("HP: ");
+      for(int i = 0; i < w.getHP()/10; i++) {
+         System.out.print("‚ñ†");
+      }
+      for(;hp < w.getMaxHP();hp += 10) {
+         System.out.print("‚ñ°");
+      }
+      System.out.print("\nMP: ");
+      for(int i = 0; i < w.getMP()/10; i++) {
+         System.out.print("‚ñ†");
+      }
+      for(;w.getMP() < w.getMaxMP();mp+=10) {
+         System.out.print("‚ñ°");
+      }
+      System.out.println("\n\n"+str);
+      System.out.print("HP: ");
+      for(int i = 0; i < m.getHP()/10; i++) {
+         System.out.print("‚ñ†");
+      }
+      hp = m.getHP();
+      for(;hp < m.getMaxHP();hp += 10) {
+         System.out.print("‚ñ°");
+      }
+      System.out.println("");
+   }
 
-	//∏ÛΩ∫≈Õø°∞‘ »Æ∑¸∑Œ æ∆¿Ã≈€¿ª æÚ¥¬ ∏ﬁº≠µÂ
-	@Override
-	public void getItemByMonster(Object o1, List<String> item) {
-		Monster m = null;
-		//ΩΩ∂Û¿”ø°∞‘ æ∆¿Ã≈€¿ª æÚ¥¬¥Ÿ∞Ì «•Ω√
-		if(o1.getClass().getName().equals("com.project1.Slime")) {
-			m = (Slime)o1;
-			System.out.println("Get item from slime");
-		}
-		int num = rand.nextInt(10);
-		System.out.println("Random Item: "+num);
-		if(num < 3) {
-			System.out.println("Get \'Hp up\'");
-			item.add("Hp up");
-		} else if (num < 6) {
-			System.out.println("Get \'Mp up\'");
-			item.add("Mp up");
-		}
-		else {
-			System.out.println("Get \'Iced\'");
-			item.add("Iced");
-		}
-	}
 
-	//¡◊¿∫∞≈ »Æ¿Œ«œ¥¬ ∏ﬁº≠µÂ.
-	@Override
-	public boolean isAlive(Object o) {
-		Monster c = null;
-		if (o.getClass().getName().equals("com.project1.Slime")) {
-			c = (Slime) o;
-			System.out.println("Slime is dead.");
-		}
-		c.setIsAlive(false);
-		return false;
-	}
 }
